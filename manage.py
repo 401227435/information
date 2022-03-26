b51abdde5fea9e6b9091d43cd56630dd90a23a69
pangcw <13630438587@163.com>
2022-03-26 22:31:38 +0800

    配置好csrf和seesion

--- manage.py
+++ manage.py
@@ -7,11 +7,14 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
+from flask_session import Session
 import redis
 """
 1、初始化
 2、抽取配置文件在config类
-3、
+3、数据库
+4、
 """
 
 app = Flask(__name__)
@@ -28,9 +31,22 @@ class Config(object):
     REDIS_PORT = 6379
 
 
+    SECRET_KEY = "EjpNVSNQTyGi1VvWECj9TvC/+kq3oujee2kTfQUs8yCM6xX9Yjq52v54g+HVoknA"
+    ...
+    # flask_session的配置信息
+    SESSION_TYPE = "redis" # 指定 session 保存到 redis 中
+    SESSION_USE_SIGNER = True # 让 cookie 中的 session_id 被加密签名处理
+    SESSION_PERMANENT = False  # 如果设置为True，则关闭浏览器session就失效。
+    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT) # 使用 redis 的实例
+    PERMANENT_SESSION_LIFETIME = 86400 # session 的有效期，单位是秒
+
+
+
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+CSRFProtect(app)
+Session(app)
 
 
 @app.route("/")
